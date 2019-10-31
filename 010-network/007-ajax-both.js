03ca1c5f912d5a8c51bd4e1cd81802b33cb863de
zongfei04 <2474954232@qq.com>
2019-10-31 18:06:12 +0800

    add

---
+++ 010-network/007-ajax-both.js
@@ -0,0 +1,46 @@
+const http = require('http');
+
+const fs = require('fs');
+
+const url = require('url');
+
+const server = http.createServer(function(req,res){
+	// console.log(req.method);
+	if(req.method == 'POST'){
+		var body = "";
+		req.on('data',function(chunk){
+			body += chunk
+		})
+		req.on('end',function(){
+			res.end(body);
+		})
+	}
+	else if(req.method == 'GET'){
+		if(req.url.search(/\?/) == -1){
+			const filePath = './' + req.url;
+			fs.readFile(filePath,function(err,data){
+				if(err){
+					res.statusCode = 404;
+					res.end('Not fond!!!!')
+				}
+				else{
+					res.end(data);
+				}
+			})
+		}
+		else{
+			const parm = url.parse(req.url,true).query;
+			// console.log(parm);
+			var strToJSON = JSON.stringify(parm);
+			res.end(strToJSON);
+		}
+		
+	}
+	else{
+		res.end('ok');
+	}
+	
+})
+server.listen(3000,'127.0.0.1',function(){
+	console.log("server is running at http://127.0.0.1:3000")
+})
