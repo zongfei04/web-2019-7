2ad66f7b4ae56751e395fec0e5e7be19e8d61a03
zongfei04 <2474954232@qq.com>
2019-12-01 11:57:28 +0800

    'add'

--- 014-react/04-tolist/src/index.js
+++ 014-react/04-tolist/src/index.js
@@ -3,8 +3,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-
+/*
 ReactDOM.render(
 	<App />,
 	document.getElementById('root')
 );
+*/
+//虚拟DOM
+function tick(){
+	const timer = (
+		<div>
+			<h1>hello world</h1>
+			<p>{new Date().toLocaleString()}</p>
+		</div>
+	)
+	ReactDOM.render(
+		timer,
+		document.getElementById('root')
+	)
+}
+setInterval(tick,1000)
