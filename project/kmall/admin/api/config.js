cbb6f0c225770475360aeb665a3de02b2bf13152
zongfei04 <2474954232@qq.com>
2019-12-11 20:59:24 +0800

    'add'

---
+++ project/kmall/admin/api/config.js
@@ -0,0 +1,8 @@
+
+
+export const SERVER = '//127.0.0.1:3000'
+
+export const API_CONFIG = {
+	
+	login:['/sessions/users','post']
+}
--- project/kmall/admin/src/pages/login/store/actionCreator.js
+++ project/kmall/admin/src/pages/login/store/actionCreator.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 import * as types  from './actionTypes.js'
 
+import {message} from 'antd'
+
 
 const getLoadInitDataAction = (payload)=>({
     type:types.LOAD_DATA,
@@ -19,9 +21,19 @@ export const getLoginItem = (values)=>{
         })
         .then(result=>{
             console.log(result)
+            const data = result.data
+            if(data.code == 0){
+                //登录成功
+                //1.将用户信息保存在前台
+                //2.返回到后台首页
+                
+            }
+            else{//登录失败
+                message.error(data.message)
+            }
         })
         .catch(err=>{
-            console.log(err)
+            message.error('请求失败，请稍后再试')
         })        
     }
 }
